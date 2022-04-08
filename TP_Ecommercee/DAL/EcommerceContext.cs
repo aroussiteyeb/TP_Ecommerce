fab265d46b2046bc01cc1413a67218aaf6f55b3e
aroussiteyeb <aroussi1996@gmail.com>
2022-04-08 14:26:20 +0200

    Add project files.

---
+++ TP_Ecommercee/DAL/EcommerceContext.cs
@@ -0,0 +1,15 @@
+﻿using TP_Ecommerce.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+namespace TP_Ecommerce.DAL
+{
+    public class EcommerceContext : DbContext
+    {
+        public EcommerceContext() : base("EcommerceContext")
+        {
+        }
+        public DbSet<Produit> produit { get; set; }
+    public DbSet<Marque> marque { get; set; }
+public DbSet<Categorie> categorie{ get; set; }
+ }
+}
